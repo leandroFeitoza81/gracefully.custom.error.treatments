7c71277010aa287d9b6141dcf0921e5c24c33638
leandro feitoza <leandro.feitoza@proton.me>
2025-02-14 13:54:03 -0300

    setup inicial
    # Conflicts:
    #	src/CodeMasterDev.Api/CodeMasterDev.Api.csproj
    #	src/gracefully.custom.error.treatments.sln

--- src/CodeMasterDev.Api/Program.cs
+++ src/CodeMasterDev.Api/Program.cs
@@ -5,9 +5,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        
+        
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseHttpsRedirection();
+        app.MapControllers();
 
         app.Run();
     }
