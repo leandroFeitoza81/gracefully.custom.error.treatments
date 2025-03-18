7ccde5d16c1df8256c8ee8c3ed84a9e295ebf29f
leandro.feitoza <leandro.feitoza@webmotors.com.br>
2025-03-18 16:02:52 -0300

    Adicionar classes ControllerSuper e ObjectResponse
    Este commit introduz a classe `ControllerSuper` no namespace `CodeMasterDev.Api.Controllers`, que fornece métodos estáticos para gerar respostas HTTP padronizadas, incluindo `CustomResponse`, `ResponseOk`, `ResponseServerError` e `ResponseBadRequest`. Além disso, uma nova classe `ObjectResponse` é adicionada no namespace `CodeMasterDev.Core.Models`, definindo uma estrutura para o objeto de resposta com propriedades para status de sucesso, mensagem e dados.

---
+++ src/CodeMasterDev.Api/Controllers/ControllerSuper.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using CodeMasterDev.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeMasterDev.Api.Controllers
+{
+    public class ControllerSuper : ControllerBase
+    {
+        public static ActionResult CustomResponse(
+            HttpStatusCode statusCode,
+            bool success,
+            string? message = null,
+            object? data = null
+            )
+        {
+            var objeto = new ObjectResponse()
+            {
+                Success = success,
+                Message = message,
+                Data = data
+            };
+
+            return new ObjectResult(objeto) { StatusCode = (int)statusCode };
+        }
+
+        public static ActionResult ResponseOk(object data)
+        {
+            return CustomResponse(HttpStatusCode.OK, true, null, data);
+        }
+
+        public static ActionResult ResponseServerError(Exception ex)
+        {
+            return CustomResponse(HttpStatusCode.InternalServerError, false, ex.Message);
+        }
+
+        public static ActionResult ResponseBadRequest(string message)
+        {
+            return CustomResponse(HttpStatusCode.BadRequest, false, message);
+        }
+    }
+}
